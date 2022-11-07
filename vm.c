acb83b5aabab3394deaa03fa3d194be7de19bc53
Octavian Voicu <octavian.voicu@gmail.com>
2022-11-06 16:21:02 -0800

    Implement dummy RNG.

--- vm.c
+++ vm.c
@@ -53,6 +53,7 @@ void vm_init(struct vm_state *vm)
 	vm->reg_ser_ctrl = SERIAL_BAUD_9600;
 	vm->reg_auto_off = 0x2;
 	vm->reg_dimmer = 0xf;
+	vm->reg_random = 0x4; /* See https://xkcd.com/221/. */
 
 	get_time(&vm->t_start);
 }
