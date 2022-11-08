voctav/voja4_nibbler

commit 35f54aed39b1c58de712207015f2a5c62b9df252

Update carry flags for OR/AND/XOR ops with literal.

ops.c
@@ -333,7 +333,9 @@ void op_or(const struct vm_instruction *instr, const struct instruction_descript
 	uint8_t result = vm->user_mem[dst];
 	result |= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
-	update_flags(result, FLAG_ZERO, vm);
+	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
+		vm->reg_flags |= FLAG_CARRY;
+	}
 }
 
 void op_and(const struct vm_instruction *instr, const struct instruction_descriptor *descr, struct vm_state *vm)
@@ -342,7 +344,9 @@ void op_and(const struct vm_instruction *instr, const struct instruction_descrip
 	uint8_t result = vm->user_mem[dst];
 	result &= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
-	update_flags(result, FLAG_ZERO, vm);
+	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
+		vm->reg_flags &= ~FLAG_CARRY;
+	}
 }
 
 void op_xor(const struct vm_instruction *instr, const struct instruction_descriptor *descr, struct vm_state *vm)
@@ -351,7 +355,9 @@ void op_xor(const struct vm_instruction *instr, const struct instruction_descrip
 	uint8_t result = vm->user_mem[dst];
 	result ^= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
-	update_flags(result, FLAG_ZERO, vm);
+	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
+		vm->reg_flags ^= FLAG_CARRY;
+	}
 }
 
 void op_mov(const struct vm_instruction *instr, const struct instruction_descriptor *descr, struct vm_state *vm)
@@ -569,9 +575,9 @@ const struct instruction_descriptor INSTRUCTIONS_WIDE[] = {
 	{.op = &OP_INC,  .dst = &DST_RY,                   .flg = OP_FLAG_CAN_JUMP},
 	{.op = &OP_DEC,  .dst = &DST_RY,                   .flg = OP_FLAG_CAN_JUMP},
 	{.op = &OP_DSZ,  .dst = &DST_RY},
-	{.op = &OP_OR,   .dst = &DST_R0,  .src = &SRC_N},
-	{.op = &OP_AND,  .dst = &DST_R0,  .src = &SRC_N},
-	{.op = &OP_XOR,  .dst = &DST_R0,  .src = &SRC_N},
+	{.op = &OP_OR,   .dst = &DST_R0,  .src = &SRC_N,   .flg = OP_FLAG_UPDATE_CARRY},
+	{.op = &OP_AND,  .dst = &DST_R0,  .src = &SRC_N,   .flg = OP_FLAG_UPDATE_CARRY},
+	{.op = &OP_XOR,  .dst = &DST_R0,  .src = &SRC_N,   .flg = OP_FLAG_UPDATE_CARRY},
 	{.op = &OP_EXR,                   .src = &SRC_N},
 	{.op = &OP_BIT,  .dst = &DST_RGI, .src = &SRC_M},
 	{.op = &OP_BSET, .dst = &DST_RGO, .src = &SRC_M},

ops.h
@@ -43,6 +43,7 @@ enum {
 	OP_FLAG_CAN_JUMP = 0x2,
 	OP_FLAG_CAN_RD_SFR = 0x4,
 	OP_FLAG_CAN_WR_SFR = 0x8,
+	OP_FLAG_UPDATE_CARRY = 0x10,
 };
 
 struct instruction_descriptor {

commit f69a03c8833b0c5f1f10433ff4b320138e9c5e4b

Update zero flag in OR/AND/XOR.
Reverts part of 35f54ae, which mistakenly removed this when implementing
carry flag setting.

ops.c
@@ -341,6 +341,7 @@ void op_or(const struct vm_instruction *instr, const struct instruction_descript
 	uint8_t result = vm->user_mem[dst];
 	result |= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
+	update_flags(result, FLAG_ZERO, vm);
 	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
 		vm->reg_flags |= FLAG_CARRY;
 	}
@@ -352,6 +353,7 @@ void op_and(const struct vm_instruction *instr, const struct instruction_descrip
 	uint8_t result = vm->user_mem[dst];
 	result &= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
+	update_flags(result, FLAG_ZERO, vm);
 	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
 		vm->reg_flags &= ~FLAG_CARRY;
 	}
@@ -363,6 +365,7 @@ void op_xor(const struct vm_instruction *instr, const struct instruction_descrip
 	uint8_t result = vm->user_mem[dst];
 	result ^= descr->src->get_val(instr, vm);
 	vm->user_mem[dst] = result;
+	update_flags(result, FLAG_ZERO, vm);
 	if (descr->flg & OP_FLAG_UPDATE_CARRY) {
 		vm->reg_flags ^= FLAG_CARRY;
 	}